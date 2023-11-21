fbca5ae0b3429ca0222438d3f421a6a9a0e33e5e
Minseo Kim <minskim010203@gmail.com>
2023-11-21 21:57:30 +0900

    SIFT extractor

--- models/ClassicalModel.py
+++ models/ClassicalModel.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
-
+import cv2
+import numpy as np
 
 class SobelEdgeDetector(nn.Module):
     def __init__(self):
@@ -26,6 +27,54 @@ class SobelEdgeDetector(nn.Module):
 
         edge_strength = torch.sqrt(x_sobel_x**2 + x_sobel_y**2)
         return edge_strength
+
+
+class SIFTFeatureExtractor(nn.Module):
+    def __init__(self, num_keypoints=133):
+        super(SIFTFeatureExtractor, self).__init__()
+        self.num_keypoints = num_keypoints
+        self.sift = cv2.SIFT_create(self.num_keypoints)
+
+    def forward(self, x):
+        # Assuming x is a CPU tensor of shape (batch_size, channels, height, width)
+        batch_size, channels, height, width = x.shape
+        x_np = x.numpy().transpose(0, 2, 3, 1)  # Convert to numpy and shape (batch_size, height, width, channels)
+
+        # Placeholder for SIFT features
+        sift_features = []
+
+        for i in range(batch_size):
+            # Convert to grayscale
+            gray_image = cv2.cvtColor(x_np[i], cv2.COLOR_BGR2GRAY)
+            # Detect SIFT features and compute descriptors.
+            keypoints, descriptors = self.sift.detectAndCompute(gray_image, None)
+
+            # Here you need to decide how to handle the keypoints and descriptors.
+            # For instance, you can create a fixed-size feature map or a tensor based on the descriptors
+            # You might need to normalize or otherwise process the descriptors before using them
+
+            # Example: Create a fixed-size padded descriptor tensor
+            if descriptors is not None:
+                if descriptors.shape[0] < self.num_keypoints:
+                    # If we have fewer than num_keypoints descriptors, pad with zeros
+                    descriptors = np.vstack(
+                        [descriptors, np.zeros((self.num_keypoints - descriptors.shape[0], descriptors.shape[1]))]
+                    )
+                elif descriptors.shape[0] > self.num_keypoints:
+                    # If we have more than num_keypoints descriptors, truncate
+                    descriptors = descriptors[:self.num_keypoints, :]
+            else:
+                # If no descriptors were found, create a zero tensor
+                descriptors = np.zeros((self.num_keypoints, 128))  # 128 is the length of SIFT descriptors
+
+            sift_features.append(descriptors)
+
+        # Convert list of numpy arrays to a single tensor
+        sift_tensor = torch.tensor(np.stack(sift_features), dtype=torch.float32)
+
+        # Now you have a batch of SIFT descriptors as a tensor
+        return sift_tensor
+
 
 
 if __name__ == "__main__":
