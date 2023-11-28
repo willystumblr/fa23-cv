258a6fd23fbc29c500123c4d34cb020eef3b0711
Minseo Kim <minskim010203@gmail.com>
2023-11-29 01:00:11 +0900

    resnet18_with_sift

--- models/CombinedModel.py
+++ models/CombinedModel.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from .Resnet50 import model_resnet50
+from .Resnet50 import model_resnet50, model_resnet18
 from .ClassicalModel import SobelEdgeDetector
 
 
@@ -197,6 +197,36 @@ class model_resnet50_with_sift(nn.Module):
         # Final prediction
         output = self.fc(combined_features)
         return output
+class model_resnet18_with_sift(nn.Module):
+    def __init__(self, weights=None, num_keypoints=133, num_sift_features=128):
+        super(model_resnet18_with_sift, self).__init__()
+        
+        self.resnet = model_resnet18(weights=weights)
+        # Define a processing layer for SIFT features
+        self.sift_processor = nn.Sequential(
+            nn.Linear(num_keypoints * num_sift_features, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU()
+        )
+        # Final fully connected layer that combines both streams
+        self.fc = nn.Linear(512 + (num_keypoints * 3), num_keypoints * 3)
+        
+    def forward(self, x, sift_features):
+        # Process the image with the ResNet stream
+        image_features = self.resnet(x)
+
+        # Flatten and process the SIFT features
+        # sift_features = sift_features.view(sift_features.size(0), -1)
+        sift_features = self.sift_processor(sift_features)
+
+        # Concatenate the image and SIFT features
+        combined_features = torch.cat((image_features, sift_features), dim=1)
+
+        # Final prediction
+        output = self.fc(combined_features)
+        return output
+
 
 class model_resnet50_4_with_sobel(nn.Module):
     def __init__(self, weights=None):
